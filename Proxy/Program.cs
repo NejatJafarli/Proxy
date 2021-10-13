607105187a43f9c96fc668117cc882252df2827d
nicat ceferli <mega.ceferli@gmail.com>
2021-10-13 19:23:45 +0400

    push

---
+++ Proxy/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Proxy
+{
+    //Cache Proxy
+    abstract class CalculatorBase
+    {
+        public abstract int Calculator();
+    }
+
+    class CalculatorManager : CalculatorBase
+    {
+        public override int Calculator()
+        {
+            return 1 + 1;
+        }
+    }
+
+    class CalculatorProxyServer : CalculatorBase
+    {
+        private CalculatorManager _calculatorManager;
+        private int _cacheValue = 0;
+        public override int Calculator()
+        {
+            if (_calculatorManager==null)
+            {
+                _calculatorManager = new CalculatorManager();
+                _cacheValue = _calculatorManager.Calculator();
+            }
+            return _cacheValue;
+        }
+    }
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            CalculatorBase calculatorBase = new CalculatorProxyServer();
+            Console.WriteLine(calculatorBase.Calculator());
+            Console.WriteLine(calculatorBase.Calculator());
+
+        }
+    }
+}
---
+++ Proxy2/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+
+namespace Proxy2
+{
+    //protected Proxy
+    interface IBanka
+    {
+        bool OdemeYap(double Tutar);
+    }
+
+    class Banka : IBanka
+    {
+        public bool OdemeYap(double Tutar)
+        {
+            if (Tutar < 100)
+                Console.WriteLine($"Ödeyeceğiniz tutar 100 TL'den az olamaz. Fark -> { 100 - Tutar }");
+            else if (Tutar > 100)
+                Console.WriteLine($"Ödeyeceğiniz tutar 100 TL'den fazla olamaz. Fark -> { Tutar - 100 }");
+            else
+            {
+                Console.WriteLine($"Ödeme başarıyla gerçekleştirildi. -> { Tutar }");
+                return true;
+            }
+
+            return false;
+        }
+    }
+    class ProxyBanka : IBanka
+    {
+        Banka banka;
+
+
+        bool Login;
+        string KullaniciAdi, Sifre;
+        public ProxyBanka(string KullaniciAdi, string Sifre)
+        {
+            this.KullaniciAdi = KullaniciAdi;
+            this.Sifre = Sifre;
+        }
+
+        bool GirisYap()
+        {
+            if (KullaniciAdi.Equals("nicat") && Sifre.Equals("nicat"))
+            {
+
+                //Protected Proxy Start
+                banka = new Banka();
+                Login = true;
+                Console.WriteLine("Hesaba giriş yapıldı.");
+                return true;
+            }
+            else
+                Console.WriteLine("Lütfen kullanıcı adı ve şifreinizi doğru girdiğinize emin olunuz.");
+
+            Login = false;
+            return false;
+        }
+
+        public bool OdemeYap(double Tutar)
+        {
+            GirisYap();
+
+            if (!Login)
+            {
+                Console.WriteLine("Hesaba giriş yapılmadığından dolayı ödeme alamıyoruz.");
+                return false;
+            }
+
+            banka.OdemeYap(Tutar);
+            return true;
+        }
+    }
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string KullaniciAdi = "", Sifre = "";
+            double Tutar = 0;
+            while (true)
+            {
+                Console.WriteLine("Lütfen kullanıcı adınızı giriniz.");
+                KullaniciAdi = Console.ReadLine();
+
+                Console.WriteLine("Lütfen şifrenizi giriniz.");
+                Sifre = Console.ReadLine();
+
+                Console.WriteLine("Lütfen ödenecek miktarı giriniz.");
+                Tutar = Convert.ToInt32(Console.ReadLine());
+
+                IBanka banka = new ProxyBanka(KullaniciAdi, Sifre);
+                banka.OdemeYap(Tutar);
+
+                Console.WriteLine("************");
+            }
+        }
+    }
+}
